3bd0131c159638ec683652382b9e95c3414ccae1
selim121 <selimkhan.bdkhan@gmail.com>
2023-05-02 18:10:41 +0600

    sign in with google added

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBpx6Hw1y7SKOgfB74psJd-wqMD5dsEFYQ",
+  authDomain: "zestful-d961f.firebaseapp.com",
+  projectId: "zestful-d961f",
+  storageBucket: "zestful-d961f.appspot.com",
+  messagingSenderId: "817225277702",
+  appId: "1:817225277702:web:b76a27c34c179f63bdbe87"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -4,47 +4,71 @@ import signIn from '../../images/signin.svg';
 import google from '../../images/logo/google.svg';
 import github from '../../images/logo/github.svg';
 import './SignIn.css';
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import app from '../../firebase/firebase.init';
 
 const SignIn = () => {
+
+    const auth = getAuth(app);
+    const provider = new GoogleAuthProvider();
+
+    const handleGoogleSignIn = () => {
+        signInWithPopup(auth, provider)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+        })
+        .catch(error => {
+            console.log('error', error.message);
+        })
+    }
+
+
     return (
         <>
             <div className="row">
-                <div className="col-lg-6 col-md-12 d-flex align-items-center">
-                    <form className='w-50 mx-auto'>
-                        <div className="text-center">
-                            <p className="fs-1 fw-bold m-0 color-primary">Zestful</p>
-                            <p>Sign In</p>
-                        </div>
-                        <div className="mb-3">
-                            <label className="form-label">Email address</label>
-                            <input type="email" className="form-control" placeholder='Your email address' aria-describedby="emailHelp" />
-                        </div>
-                        <div className="mb-3">
-                            <label className="form-label">Password</label>
-                            <input type="password" className="form-control" placeholder='Your password' />
-                        </div>
-                        <button type="submit" className="btn btn-warning text-white fw-bold w-100">Sign In</button>
-                        <div className="mt-2 text-end">
-                            <p>Don`t have an account? <Link className='color-primary sign-up-link' to='/sign-up'>Sign Up</Link></p>
-                        </div>
-                        <div className="container my-3">
-                            <div className="row">
-                                <div className="col-5">
-                                    <hr className="my-2" />
-                                </div>
-                                <div className="col-2 text-center">
-                                    <span className="px-2">or</span>
-                                </div>
-                                <div className="col-5">
-                                    <hr className="my-2" />
+                <div className="col-lg-6 col-md-12 my-auto">
+                    <div className="">
+                        <form className='w-50 mx-auto'>
+                            <div className="text-center">
+                                <p className="fs-1 fw-bold m-0 color-primary">Zestful</p>
+                                <p>Sign In</p>
+                            </div>
+                            <div className="mb-3">
+                                <label className="form-label">Email address</label>
+                                <input type="email" className="form-control" placeholder='Your email address' aria-describedby="emailHelp" />
+                            </div>
+                            <div className="mb-3">
+                                <label className="form-label">Password</label>
+                                <input type="password" className="form-control" placeholder='Your password' />
+                            </div>
+                            <button type="submit" className="btn btn-warning text-white fw-bold w-100">Sign In</button>
+                            <div className="mt-2 text-end">
+                                <p>Don`t have an account? <Link className='color-primary sign-up-link' to='/sign-up'>Sign Up</Link></p>
+                            </div>
+                            <div className="container my-3">
+                                <div className="row">
+                                    <div className="col-5">
+                                        <hr className="my-2" />
+                                    </div>
+                                    <div className="col-2 text-center">
+                                        <span className="px-2">or</span>
+                                    </div>
+                                    <div className="col-5">
+                                        <hr className="my-2" />
+                                    </div>
                                 </div>
                             </div>
+                        </form>
+                        <div className="text-center">
+                            <div className="my-5">
+                                <Link onClick={handleGoogleSignIn} className="sign-in-btn color-primary"><img src={google} className="icon me-2" /> Sign in with Google</Link>
+                            </div>
+                            <div className="">
+                                <Link className="sign-in-btn color-primary"><img src={github} alt="" className="icon me-2" /> Sign in with GitHub</Link>
+                            </div>
                         </div>
-
-                        <button type="submit" className="sign-in-btn w-100 mb-3 color-primary"><img src={google} alt="" className="icon me-2" /> Sign in with Google</button>
-                        <button type="submit" className="sign-in-btn w-100 color-primary"><img src={github} alt="" className="icon me-2" /> Sign in with GitHub</button>
-
-                    </form>
+                    </div>
                 </div>
                 <div className="col-lg-6 col-md-12 h-50">
                     <img className='img-fluid h-75' src={signIn} alt="" />
